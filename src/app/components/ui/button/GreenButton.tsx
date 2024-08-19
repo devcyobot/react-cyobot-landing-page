f5a4dad678846bfef8a79c4ec38693cdedb4e175
tngPhVN <tnphan@ucsd.edu>
2024-08-19 10:39:41 -0700

    edit GreenButton

--- src/app/components/ui/button/GreenButton.tsx
+++ src/app/components/ui/button/GreenButton.tsx
@@ -4,6 +4,8 @@ import { FC } from "react";
 type GreenButtonProps = {
 	link?: string;
 	description: string;
+	type?: "button" | "submit";
+	disabled?: boolean;
 };
 
 export const GreenButton: FC<GreenButtonProps> = (props) => {
@@ -20,7 +22,8 @@ export const GreenButton: FC<GreenButtonProps> = (props) => {
 				</Link>
 			) : (
 				<button
-					type="button"
+					aria-disabled={props.disabled}
+					type={props.type}
 					className="rounded-lg bg-brand-green text-white md:text-xl xl:text-2xl font-normal p-3 w-full"
 				>
 					{props.description}
