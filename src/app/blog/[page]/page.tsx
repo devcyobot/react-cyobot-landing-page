996c3f4a678f0dcaf710e273a7b2e421c59779a7
tngPhVN <tnphan@ucsd.edu>
2024-10-21 19:31:54 -0700

    fix pagination bug

--- src/app/blog/[page]/page.tsx
+++ src/app/blog/[page]/page.tsx
@@ -1,23 +1,17 @@
 import BlogList from "@/app/blog/BlogList";
 import Pagination from "@/app/components/ui/Pagination";
-import { BLOG_PER_PAGE, useBlogByPage } from "@/app/hooks/use-blog-by-page";
-import { useCountBlog } from "@/app/hooks/use-count-blog";
+import { useBlogByPage } from "@/app/hooks/use-blog-by-page";
 
 const Blog = async ({ params }: { params: { page: string } }) => {
 	const currentPage = parseInt(params.page, 10) || 1;
-	const blogCount = await useCountBlog();
-	const totalPages =
-		blogCount !== null ? Math.ceil(blogCount / BLOG_PER_PAGE) : 0;
-	const totalBlog = blogCount !== null ? blogCount : 0;
-	const fetchedBlog = await useBlogByPage(currentPage, totalBlog);
-	console.log(fetchedBlog);
+	const { blogsPerPage, totalPages } = await useBlogByPage(currentPage);
 
 	return (
 		// <main className="mx-auto min-h-[85vh] max-w-[1536px] bg-brand-dark-2 grid lg:grid-cols-[1fr_4fr] w-full pt-16 md:pt-24 px-6 sm:px-10 lg:px-20 gap-10">
 		<main className="w-4/5 sm:w-[70%] mx-auto min-h-[85vh] max-w-[1536px] bg-brand-dark-2 grid lg:grid-cols-1 pt-16 md:pt-24 gap-10">
 			{/* <FilterSearchBar data={["dev"]} /> */}
 			<section className="flex h-full flex-col justify-between w-full mx-auto gap-6">
-				<BlogList data={fetchedBlog} />
+				<BlogList data={blogsPerPage} />
 				<Pagination currentPage={currentPage} totalPages={totalPages} />
 			</section>
 		</main>
--- src/app/hooks/use-blog-by-page.ts
+++ src/app/hooks/use-blog-by-page.ts
@@ -1,46 +1,25 @@
 import { WebsiteBlog } from "@/app/types";
 import qs from "qs";
 
-export const BLOG_PER_PAGE = 5;
+export const BLOG_PER_PAGE = 10;
 
 export const useBlogByPage = async (
-	currentPage: number,
-	totalBlog: number
-): Promise<WebsiteBlog[]> => {
+	currentPage: number
+): Promise<{
+	blogsPerPage: WebsiteBlog[];
+	totalPages: number;
+}> => {
 	try {
-		const maxQuantile = Math.ceil(totalBlog / BLOG_PER_PAGE);
-		if (currentPage > maxQuantile) {
-			throw new Error(
-				`Quantile ${currentPage} exceeds the total available pages.`
-			);
-		}
-
-		const offset = (currentPage - 1) * BLOG_PER_PAGE;
-		const itemsLeft = totalBlog - offset;
-		const fetchLimit = itemsLeft < BLOG_PER_PAGE ? itemsLeft : BLOG_PER_PAGE;
-
 		const blogResponse = await fetch(
 			`${process.env.NEXT_PUBLIC_DASHBOARD_URL}/api/website-blogs?${qs.stringify(
 				{
 					sort: "-publishedDate",
-					offset,
-					limit: fetchLimit,
+					page: currentPage,
+					limit: BLOG_PER_PAGE,
 					where: { _status: { equals: "published" } },
 				}
 			)}`,
-			// { next: { revalidate: 5 } }
-			{ cache: "no-store" }
-		);
-
-		console.log(
-			`${process.env.NEXT_PUBLIC_DASHBOARD_URL}/api/website-blogs?${qs.stringify(
-				{
-					sort: "-publishedDate",
-					offset,
-					limit: fetchLimit,
-					where: { _status: { equals: "published" } },
-				}
-			)}`
+			{ next: { revalidate: 5 } }
 		);
 
 		if (!blogResponse.ok) {
@@ -55,9 +34,12 @@ export const useBlogByPage = async (
 			throw new Error("Invalid blog data format.");
 		}
 
-		return fetchedBlog.docs as WebsiteBlog[];
+		return {
+			blogsPerPage: fetchedBlog.docs as WebsiteBlog[],
+			totalPages: fetchedBlog.totalPages as number,
+		};
 	} catch (error) {
 		console.error("Failed to fetch blog data:", error);
-		return [];
+		return { blogsPerPage: [], totalPages: 0 };
 	}
 };
