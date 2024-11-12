5d94d944f323fd955581f7230df0b41c1b69e9cf
tngPhVN <tnphan@ucsd.edu>
2024-11-11 20:03:30 -0800

    edit button position in Banner

--- src/app/home/Banner.tsx
+++ src/app/home/Banner.tsx
@@ -23,7 +23,7 @@ const Banner: FC = () => {
 			</Link>
 			<CustomLink
 				variant={"shadow"}
-				className="mb-3 w-fit bg-brand-purple inset-y-[55%] left-[11%] 2xl:left-[20%] md:absolute py-7 px-12 font-roboto text-white text-lg lg:text-2xl"
+				className="mb-3 w-fit bg-brand-purple inset-y-[55%] left-[11%] 2xl:left-[15%] md:absolute py-7 px-12 font-roboto text-white text-lg lg:text-2xl"
 				target="_blank"
 				href="https://www.kickstarter.com/projects/cyobot/cyobot-a-transformable-quadruped-robot-for-innovation-and-fun?fbclid=IwZXh0bgNhZW0CMTEAAR2YwQX2DR0nwytx89OG8r_AO-y_EGh0gTWCmWQxcPi8BlKFY48NkLhAUaw_aem_ad7em1x9laTtyPn0fX4y5w"
 			>
